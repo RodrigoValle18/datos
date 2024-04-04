aa95681cc450f62b0bb5135c068a9daeffa80da0
Rodrigo Diez Valle <rodrigodiezvalle@gmail.com>
2024-04-04 11:35:21 +0200

    modulo 2

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import "./style.css";
+
+console.log("Hola Mundo");
+console.log("Hola JavaScript");
+console.log(53-17);
+console.log(3.14*(4**2));
+console.log("Cuenta de cena de cumpleaños");
+    console.log( (120-18)/7)
+        console.log("6 amigos más el anfitrión")
